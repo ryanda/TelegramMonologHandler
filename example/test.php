7bd05bb93d9827bafaef81b8453f20453b6ee320
ryanda <ryandaputra@gmail.com>
2020-05-29 08:44:55 +0700

    remove

--- example/test.php
+++ example/test.php
@@ -7,7 +7,7 @@ use Ryanda\MonologTelegram\TelegramBotHandler;
 
 // create a log channel
 $log = new Logger('name');
-$log->pushHandler(new TelegramBotHandler('***REMOVED***', ***REMOVED***, Logger::WARNING));
+$log->pushHandler(new TelegramBotHandler('YOUR_API_KEY', 'YOUR_CHANNEL_ID', Logger::WARNING));
 
 // push to the channel
 $log->warning('Foo');
