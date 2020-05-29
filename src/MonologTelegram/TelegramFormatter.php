5a3c835168c99007f95e5809fbb1014d84a50f59
ryanda <ryandaputra@gmail.com>
2020-05-29 08:29:27 +0700

    basic formatter

---
+++ src/MonologTelegram/TelegramFormatter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ryanda\MonologTelegram;
+
+use Monolog\Formatter\NormalizerFormatter;
+
+class TelegramFormatter extends NormalizerFormatter
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function format(array $record)
+    {
+        $record = parent::format($record);
+
+        return $this->getException($record);
+    }
+
+    /**
+     * Generate custom format message from exception log
+     * @param array $record
+     * @return string
+     */
+    protected function getException(array $record)
+    {
+        $record['level'] = strtolower($record['level_name']);
+        $record['description'] = $record['message'];
+
+        return sprintf('%s: ```%s```', strtoupper($record['level']), $record['description']);
+    }
+}
